c664e5dd811c7fe40ee3451f7cc7dce835c451c1
alancodvo <alancodvo@gmail.com>
2015-02-07 10:55:56 +0900

    Initial Commit

---
+++ SwiftOneDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftOneDay
+//
+//  Created by alan-arakawa-yoshihiro on H27/02/07.
+//  Copyright (c) 平成27年 Yoshihiro Arakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
